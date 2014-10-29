64e609a7ad39ca5b0449811d749629f566e18070
Thomas ten Cate <ttencate@gmail.com>
2014-10-29 13:11:59 +0100

    Implement line splitting and tab replacement

--- src/github.com/ttencate/commonmark/commonmark.go
+++ src/github.com/ttencate/commonmark/commonmark.go
@@ -2,12 +2,96 @@
 // HTML.
 package commonmark
 
-// ToHTML converts a string formatted in CommonMark into the corresponding HTML
-// string.
+import (
+  "bufio"
+  "bytes"
+)
+
+// ToHTMLBytes converts text formatted in CommonMark into the corresponding
+// HTML.
 //
-// Note that it might contain unsafe tags (e.g. <script>); if you are accepting
-// untrusted user input, you must run the output through a sanitizer before
-// sending it to a browser.
-func ToHTML(markdown string) string {
-  return markdown
+// The input must be encoded as UTF-8.
+//
+// Line breaks in the output will be single '\n' bytes, regardless of line
+// endings in the input (which can be CR, LF or CRLF).
+//
+// Note that the output might contain unsafe tags (e.g. <script>); if you are
+// accepting untrusted user input, you must run the output through a sanitizer
+// before sending it to a browser.
+func ToHTMLBytes(markdown []byte) ([]byte, error) {
+  scanner := bufio.NewScanner(bytes.NewReader(markdown))
+  scanner.Split(scanLines)
+  var html []byte
+  for scanner.Scan() {
+    line := scanner.Bytes()
+    line = tabsToSpaces(line)
+
+    html = append(html, line...)
+    html = append(html, '\n')
+  }
+
+  if err := scanner.Err(); err != nil {
+    return nil, err
+  }
+
+  return html, nil
+}
+
+// scanLines is a split function for bufio.Scanner that splits on CR, LF or
+// CRLF pairs. We need this because bufio.ScanLines itself only does CR and
+// CRLF.
+func scanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+  if atEOF && len(data) == 0 {
+    return 0, nil, nil
+  }
+  for i := 0; i < len(data); i++ {
+    if data[i] == '\r' {
+      if i + 1 < len(data) && data[i + 1] == '\n' {
+        return i + 2, data[0:i], nil
+      } else {
+        return i + 1, data[0:i], nil
+      }
+    } else if data[i] == '\n' {
+      return i + 1, data[0:i], nil
+    }
+  }
+  // If we're at EOF, we have a final, non-terminated line. Return it.
+  if atEOF {
+    return len(data), data, nil
+  }
+  // Request more data.
+  return 0, nil, nil
+}
+
+// tabsToSpaces returns a slice (possibly the same one) in which tabs have been
+// replaced by up to 4 spaces, depending on the tab stop position.
+//
+// It does not modify the input slice; a copy is made if needed.
+func tabsToSpaces(line []byte) []byte {
+  const tabStop = 4
+
+  var tabCount int
+  for remaining := line; len(remaining) > 0; {
+    i := bytes.IndexByte(remaining, '\n')
+    if i == -1 {
+      break
+    }
+    remaining = remaining[i + 1:]
+    tabCount++
+  }
+  if tabCount == 0 {
+    return line
+  }
+
+  output := make([]byte, 0, len(line) + 4 * tabCount)
+  for i := 0; i < len(line); i++ {
+    if line[i] == '\t' {
+
+      spaces := bytes.Repeat([]byte{' '}, tabStop - i % tabStop)
+      output = append(output, spaces...)
+    } else {
+      output = append(output, line[i])
+    }
+  }
+  return output
 }
--- src/github.com/ttencate/commonmark/spec_test.go
+++ src/github.com/ttencate/commonmark/spec_test.go
@@ -3,33 +3,44 @@ package commonmark
 import (
   "bufio"
   "go/build"
+  "bytes"
   "path/filepath"
   "io"
+  "log"
   "os"
   "testing"
 )
 
 type example struct {
-  input string
-  output string
+  input []byte
+  output []byte
 }
 
 func TestSpec(t *testing.T) {
   specFile, err := openSpecFile()
   if err != nil {
-    t.Fatalf("Failed to load spec.txt: %s", err)
+    t.Fatalf("error loading spec.txt: %s", err)
   }
 
   examples := make(chan example)
   go readExamples(specFile, examples)
 
+  var count, failures int
   for ex := range examples {
-    actualOutput := ToHTML(ex.input)
-    if actualOutput != ex.output {
-      t.Errorf("Input:\n%s\nActual output:\n%s\nExpected output:\n%s",
-          ex.input, actualOutput, ex.output)
+    count++
+    actualOutput, err := ToHTMLBytes(ex.input)
+    if err != nil {
+      failures++
+      t.Errorf("error: %s\ninput:\n%s", err, ex.input)
+      continue
+    }
+    if !bytes.Equal(actualOutput, ex.output) {
+      failures++
+      t.Errorf("incorrect output\ninput:\n%s\nexpected output:\n%s\nactual output:\n%s",
+          ex.input, ex.output, actualOutput)
     }
   }
+  t.Logf("spec test complete\ntests run: %3d\nsuccesses: %3d\nfailures:  %3d", count, count - failures, failures)
 }
 
 func openSpecFile() (*os.File, error) {
@@ -46,7 +57,7 @@ func openSpecFile() (*os.File, error) {
 func readExamples(reader io.Reader, examples chan<- example) {
   scanner := bufio.NewScanner(reader)
 
-  // Loosely based on https://github.com/jgm/CommonMark/blob/master/spec2md.pl
+  // Loosely based on https://github.com/jgm/CommonMark/blob/master/runtests.pl
   // Example syntax is:
   //
   // .
@@ -61,13 +72,13 @@ func readExamples(reader io.Reader, examples chan<- example) {
     if len(line) == 1 && line[0] == '.' {
       switch (stage) {
       case 0:
-        input = nil
-        output = nil
         stage = 1
       case 1:
         stage = 2
       case 2:
-        examples <- example{string(input), string(output)}
+        examples <- example{replaceMagicChars(input), replaceMagicChars(output)}
+        input = nil
+        output = nil
         stage = 0
       }
     } else {
@@ -82,5 +93,15 @@ func readExamples(reader io.Reader, examples chan<- example) {
     }
   }
 
+  if err := scanner.Err(); err != nil {
+    log.Panicf("error while reading spec file: %s", err)
+  }
+
   close(examples)
+}
+
+func replaceMagicChars(text []byte) []byte {
+  text = bytes.Replace(text, []byte("→"), []byte("\t"), -1)
+  text = bytes.Replace(text, []byte("␣"), []byte(" "), -1)
+  return text
 }
